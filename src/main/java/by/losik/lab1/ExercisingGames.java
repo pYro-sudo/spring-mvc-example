be7930af1d517f41bc7c7b9b648dffe1ece9bb93
pYro-sudo <109588143+pYro-sudo@users.noreply.github.com>
2025-08-26 13:18:10 +0300

    Delete src directory

--- src/main/java/by/losik/lab1/ExercisingGames.java
+++
@@ -1,8 +0,0 @@
-package by.losik.lab1;
-
-import lombok.Data;
-
-@Data
-public class ExercisingGames {
-    private String task;
-}
--- src/main/java/by/losik/lab1/TeacherTasks.java
+++
@@ -1,7 +0,0 @@
-package by.losik.lab1;
-
-public interface TeacherTasks {
-    StudyingMaterials giveStudyingMaterials(Classroom classroom, StudyingMaterials materials);
-
-    ExercisingGames exercise(Classroom classroom, ExercisingGames exercisingGames);
-}
